087d8114bb0a6d2cee038ae51bd1b90a08854907
Rocco Howard <rocco@hnh.digital>
2017-12-24 15:44:31 +1100

    Load AutoInit asset from the loader

---
+++ config/config.default.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'AutoInit' => [HnhDigital\LaravelFrontendAssetLoader\AutoInit::class],
+];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -21,6 +21,8 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__.'/../config/config.default.php', 'frontend-assets');
+
         $this->app->bind('FrontendAsset', function () {
             return new FrontendAsset();
         });
