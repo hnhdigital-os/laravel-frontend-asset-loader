bde00c4a1b076efe95fef1cb0a81fc831c2e909f
Rocco Howard <rocco@bluora.com.au>
2018-01-02 22:51:11 +0000

    Apply fixes from StyleCI

--- js/AutoInit.js
+++ js/AutoInit.js
@@ -1,11 +1,9 @@
-
 /**
  * Frontend Assets.
  *
  * @type {Object}
  */
 $.frontendAssets = {
-
   /**
    * Stores the init functions.
    *
@@ -34,9 +32,18 @@ $.frontendAssets = {
    */
   autoInit: function() {
     $('[class*="init-"]:visible').each(function(key, element) {
-      result = $.grep($(element).attr('class').split(' '), function(s) { return s.match(new RegExp('init-')) });
+      result = $.grep(
+        $(element)
+          .attr('class')
+          .split(' '),
+        function(s) {
+          return s.match(new RegExp('init-'));
+        }
+      );
       result.forEach(function(class_name) {
-        $(element).trigger('extension::'+class_name.replace('init-', '')+'::init');
+        $(element).trigger(
+          'extension::' + class_name.replace('init-', '') + '::init'
+        );
       });
     });
 
@@ -48,11 +55,14 @@ $.frontendAssets = {
     $.frontendAssets.scripts[extension] = init_function;
     $.frontendAssets.startup[extension] = setup_function;
     $.frontendAssets.storage[extension] = {};
-    $('.init-'+extension).on('extension::bs-tooltip::init', init_function);
+    $('.init-' + extension).on('extension::bs-tooltip::init', init_function);
   },
 
   captureTrigger: function(event_name) {
-    if (typeof event_name == 'string' && event_name.match(new RegExp('^extension::(.*)::init$')) != null) {
+    if (
+      typeof event_name == 'string' &&
+      event_name.match(new RegExp('^extension::(.*)::init$')) != null
+    ) {
       var extension = event_name.replace('init-', '');
       if (typeof $.frontendAssets.startup[extension] != 'undefined') {
         $.frontendAssets.startup[extension]();
@@ -72,36 +82,45 @@ $.frontendAssets = {
     if (typeof restrict_search == 'object') {
       var search_result = $(restrict_search).find(search_criteria);
     } else if (typeof restrict_search == 'string') {
-      var search_result = $(restrict_search + ' ' + search_criteria)
+      var search_result = $(restrict_search + ' ' + search_criteria);
     } else {
       var search_result = $(search_criteria);
     }
 
     search_result.each(function(key, element) {
-      var element_init_classes = $.grep($(element).attr('class').split(' '), function(s) { return s.match(new RegExp('init-')) });
+      var element_init_classes = $.grep(
+        $(element)
+          .attr('class')
+          .split(' '),
+        function(s) {
+          return s.match(new RegExp('init-'));
+        }
+      );
       element_init_classes.forEach(function(class_name) {
         var extension = class_name.replace('init-', '');
         if (typeof $.frontendAssets.scripts[extension] != 'undefined') {
-          $(element).on('extension::'+extension+'::init', $.frontendAssets.scripts[extension]);
-          $(element).trigger('extension::'+extension+'::init');
+          $(element).on(
+            'extension::' + extension + '::init',
+            $.frontendAssets.scripts[extension]
+          );
+          $(element).trigger('extension::' + extension + '::init');
         }
       });
     });
-  }
+  },
 };
 
-(function ($) {
+(function($) {
   var trigger = $.fn['trigger'];
   $.fn['trigger'] = function(e) {
     $.frontendAssets.captureTrigger(e.type);
     return trigger.apply(this, arguments);
-  }
+  };
 })(jQuery);
-
 
 $(function() {
   $.frontendAssets.autoInit();
 });
 
 // Legacy.
-var findAndApplyScriptExtensions = findAndInit = $.frontendAssets.init;
+var findAndApplyScriptExtensions = (findAndInit = $.frontendAssets.init);
