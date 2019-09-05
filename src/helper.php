691ac1da9270dabc97873b1c70648dc24b64abb7
Rocco Howard <rocco@hnh.digital>
2019-09-05 13:18:21 +0000

    Removed helpers

--- src/helper.php
+++ src/helper.php
@@ -1,16 +1,19 @@
 <?php
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
 function hookAddClassHtmlTag($class_name)
 {
     if (strpos($class_name, 'init-') !== false) {
-        $container = studly_case(str_replace(['init-', '-'], ['', '_'], $class_name));
+        $container = Str::studly(str_replace(['init-', '-'], ['', '_'], $class_name));
         app('FrontendAsset')->package($container);
     }
 
     if (strpos($class_name, 'config-') !== false) {
         $class_name_array = explode('_', $class_name);
-        $container = studly_case(str_replace(['config-', '-'], ['', '_'], array_get($class_name_array, 0)));
-        $config = explode('-', array_get($class_name_array, 1, ''));
+        $container = Str::studly(str_replace(['config-', '-'], ['', '_'], Arr::get($class_name_array, 0)));
+        $config = explode('-', Arr::get($class_name_array, 1, ''));
         app('FrontendAsset')->package($container, $config);
     }
 }
